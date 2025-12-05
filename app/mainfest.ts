182f21ccb27d944c6695f4e57fe8d5365fd03263
MorvaiRoland <morvai11roland@gmail.com>
2025-12-05 13:32:27 +0100

    mobile layout

---
+++ app/mainfest.ts
@@ -0,0 +1,25 @@
+import { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'DriveSync',
+    short_name: 'DriveSync',
+    description: 'Saját autók és szervizkönyv kezelése',
+    start_url: '/',
+    display: 'standalone', // Ez tünteti el a böngésző keretet!
+    background_color: '#0f172a', // Slate-900 (Sötét háttér betöltéskor)
+    theme_color: '#f59e0b', // Amber-500 (A fejléc színe)
+    icons: [
+      {
+        src: '/drivesync-icon.png',
+        sizes: 'any',
+        type: 'image/png',
+      },
+      {
+        src: '/drivesync-icon.png',
+        sizes: 'any',
+        type: 'image/png',
+      },
+    ],
+  }
+}
