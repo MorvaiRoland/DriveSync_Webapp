3f6b7ccdc2cabe08a93864d9b22c511aa8737b64
MorvaiRoland <morvai11roland@gmail.com>
2025-12-22 18:34:19 +0100

    fix

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dynamicsense.app',
+  appName: 'DynamicSense',
+  webDir: 'public'
+};
+
+export default config;
