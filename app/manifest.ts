2b669feb826edb2a0132e7ba91b7062f4c93d631
MorvaiRoland <morvai11roland@gmail.com>
2026-01-10 20:36:12 +0100

    fix

--- app/manifest.ts
+++ app/manifest.ts
@@ -21,6 +21,13 @@ export default function manifest(): MetadataRoute.Manifest {
 
     icons: [
       {
+        src: '/favicon.png', // Ez a korábban mutatott 144x144-es fájlod
+        sizes: '144x144',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      
+      {
         src: '/icons/icon-512.png',
         sizes: '512x512',
         type: 'image/png',
